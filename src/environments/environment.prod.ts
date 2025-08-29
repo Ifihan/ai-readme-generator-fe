8ee55b2f96a3fd655d5e197cd4df4c9ce71bafc8
ifihan <victoriaolusheye@gmail.com>
2025-08-29 23:32:18 +0100

    fixed trailing slash error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   githubClientId: 'Ov23liUXmA6O4big0CwG',
   githubClientAppId: '1201940',
   useMockData: false,
-  apiUrl: 'https://ai-readme-generator-be-912048666815.us-central1.run.app/'
+  apiUrl: 'https://ai-readme-generator-be-912048666815.us-central1.run.app'
 };
